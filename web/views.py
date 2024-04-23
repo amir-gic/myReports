9b85d7f1238babd4e93f219a02a53dbaae94211b
Amir <Amirjabraili72@gmial.com>
2024-04-24 00:09:58 +0330

    make Report Form and save and display Reports

--- web/views.py
+++ web/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render , redirect
-from .models import Report
+from .models import Report , Task
+from django import forms
 from django.http import HttpResponse
-import datetime
+from datetime import datetime , date , time
 # Create your views here.
 def index(request):
+	tasks = Task.objects.all()
+	names = [t.name for t in tasks]
+	reports = Report.objects.all()
 	#reports = Report.objects.all()
 #	return render(request,"index.html")
-	return render(request , "reports.html")
+	return render(request , "reports.html" , {"tasks":names , "reports":reports})
 
 def save(request):
-	print(request.POST["datetime"])
-	t = datetime.datetime.strptime(request.POST["datetime"],"%H:%M").time()
-	print(t)
+	post = request.POST.copy()
+	post["time"] =	datetime.combine(date.today() , datetime.strptime(request.POST["time"],"%H:%M").time())
+	print(post)
+	form = ReportForm(post)
+	if form.is_valid():
+		form.save()
+	else:
+		print("inValid")
+	# t = datetime.datetime.strptime(request.POST["time"],"%H:%M").time()
+	# print(t)
 	return redirect("index")
+
+
+class ReportForm(forms.ModelForm):
+	class Meta:
+		model = Report
+		fields = ['task', 'time']
