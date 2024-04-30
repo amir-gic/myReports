37bf837dbe5141400f61959416bef22fb3b3badf
Amir <Amirjabraili72@gmial.com>
2024-04-30 21:00:18 +0330

    fix some bug about Report.end_time field

--- web/views.py
+++ web/views.py
@@ -7,30 +7,29 @@ from datetime import datetime , date , time
 def index(request):
 	tasks = Task.objects.all()
 	names = [t.name for t in tasks]
-	reports = Report.objects.filter(time__gte = date.today())
+	reports = Report.objects.filter(start_time__gte = date.today())
 	#reports = Report.objects.all()
-#	return render(request,"index.html")
+	#return render(request,"index.html")
 	return render(request , "reports.html" , {"tasks":names , "reports":reports})
 
 def save(request):
 	post = request.POST.copy()
 	post["start_time"] =	datetime.combine(date.today() , datetime.strptime(request.POST["start_time"],"%H:%M").time())
-	print(post)
 	form = ReportForm(post)
 	if form.is_valid():
 		form.save()
-		latest_report = Report.objects.all()
-		latest_report = latest_report[len(latest_report) - 2]
-		latest_report.end_time = datetime.now()
-		latest_report.save()
+		reports = Report.objects.all()
+		if(len(reports) > 1):
+			latest_report = reports[len(reports) - 2]
+			latest_report.end_time = post["start_time"]
+			latest_report.save()
 	else:
 		print("inValid")
-	# t = datetime.datetime.strptime(request.POST["time"],"%H:%M").time()
-	# print(t)
+	
 	return redirect("index")
 
 
 class ReportForm(forms.ModelForm):
 	class Meta:
 		model = Report
-		fields = ['task', 'start_time']
+		fields = ['task', 'start_time' , 'end_time']
