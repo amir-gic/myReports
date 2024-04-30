4e825d424b4becc112bb89c7ce0ca4a7abbf92e8
Amir <Amirjabraili72@gmial.com>
2024-04-30 11:21:21 +0330

    add new field end time to model for calculating total time and save end_time when new report made

--- web/models.py
+++ web/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class Report(models.Model):
     task = models.ForeignKey("Task",models.SET_NULL,null=True)
-    time = models.DateTimeField(blank=False , null= False)
-
+    start_time = models.DateTimeField(blank=False , null= False)
+    end_time = models.DateTimeField(blank=True , null = True)
+    def total_time(self):
+        if self.start_time and self.end_time:
+            return self.end_time - self.start_time
+        else:
+            return None
 class Task(models.Model):
     name = models.CharField(max_length=255,unique=True,primary_key = True)
--- web/views.py
+++ web/views.py
@@ -7,18 +7,22 @@ from datetime import datetime , date , time
 def index(request):
 	tasks = Task.objects.all()
 	names = [t.name for t in tasks]
-	reports = Report.objects.all()
+	reports = Report.objects.filter(time__gte = date.today())
 	#reports = Report.objects.all()
 #	return render(request,"index.html")
 	return render(request , "reports.html" , {"tasks":names , "reports":reports})
 
 def save(request):
 	post = request.POST.copy()
-	post["time"] =	datetime.combine(date.today() , datetime.strptime(request.POST["time"],"%H:%M").time())
+	post["start_time"] =	datetime.combine(date.today() , datetime.strptime(request.POST["start_time"],"%H:%M").time())
 	print(post)
 	form = ReportForm(post)
 	if form.is_valid():
 		form.save()
+		latest_report = Report.objects.all()
+		latest_report = latest_report[len(latest_report) - 2]
+		latest_report.end_time = datetime.now()
+		latest_report.save()
 	else:
 		print("inValid")
 	# t = datetime.datetime.strptime(request.POST["time"],"%H:%M").time()
@@ -29,4 +33,4 @@ def save(request):
 class ReportForm(forms.ModelForm):
 	class Meta:
 		model = Report
-		fields = ['task', 'time']
+		fields = ['task', 'start_time']
