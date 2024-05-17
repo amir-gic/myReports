78d0af611da2ad489144efafab72a3f21d7545d0
Amir <Amirjabraili72@gmial.com>
2024-05-18 01:14:46 +0330

    add model validation to check report time and sort the index report in order to start_time

--- web/views.py
+++ web/views.py
@@ -9,6 +9,7 @@ def index(request):
 	tasks = Task.objects.all()
 	names = [t.name for t in tasks]
 	reports = Report.objects.filter(start_time__gte = date.today())
+	reports = sorted(reports , key = lambda x : x.start_time)
 
 	today = DateTimeRange.today()
 	#reports = Report.objects.all()
@@ -18,6 +19,8 @@ def index(request):
 def save(request):
 	post = request.POST.copy()
 	post["start_time"] =	datetime.combine(date.today() , datetime.strptime(request.POST["start_time"],"%H:%M").time())
+	if Report.objects.filter(start_time__gte = date.today()).filter(start_time__gte = post["start_time"]).exists():
+		return HttpResponse("error")
 	form = ReportForm(post)
 	if form.is_valid():
 		form.save()
